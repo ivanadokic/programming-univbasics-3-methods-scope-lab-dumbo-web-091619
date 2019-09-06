bba376dccf7e0b198629918e43b6e280e1631a97
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-06 02:41:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 describe "link" do
   it "It's Dangerous To Go Alone! Take This."
     phrase = "It's Dangerous To Go Alone! Take This."
-    expect{link}.to output("It's-a me, Mario!\n").to_stdout
+    expect{link}.to output("It's Dangerous To Go Alone! Take This\n").to_stdout
   end
 end
