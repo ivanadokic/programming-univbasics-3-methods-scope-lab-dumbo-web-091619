0028bcae0054bb4c33ed67291fb633e22905f9e1
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-06 02:40:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,6 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
+describe "link" do
+  it "It's Dangerous To Go Alone! Take This."
+    phrase = "It's Dangerous To Go Alone! Take This."
+    expect{link}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
